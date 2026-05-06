7828a14658ad3a006dd4c1ce010a57c6e68c4686
Patrick Kalita <pkalita@lbl.gov>
2026-05-06 11:52:40 -0700

    Add `bbop-manager-sparql` package

---
+++ packages/bbop-manager-sparql/src/manager.js
@@ -0,0 +1,255 @@
+/**
+ * Manager for handling communication and callbacks with a SPARQL
+ * endpoint; also allows for use of templates.
+ *
+ * @modules bbop-manager-sparql
+ */
+
+import bbop from "bbop-core";
+import registry from "bbop-registry";
+
+import us from "underscore";
+import mustache from "mustache";
+import yaml from "yamljs";
+
+/**
+ * A manager for handling the AJAX and registry. Initial take from
+ * {module:bbop-rest-manager}.
+ *
+ * @constructor
+ * @param {String} endpoint - string for the target endpoint URL
+ * @param {Array} prefixes - a list of array pairs (e.g. [['wdt', '<http://www.wikidata.org/prop/direct/>'], ...]) that will be added to all queries, whether a template is used or not.
+ * @param {Object} response_handler - the response handler class to use for each call
+ * @param {Object} engine - Remote resource manager client to use (must be an instantiated {module:bbop-rest-manager} engine)
+ * @param {String} *optional* mode - mode control for the engine (optional)
+ * @returns {manager} a classic manager
+ */
+var manager = function (endpoint, prefixes, response_handler, engine, mode) {
+  registry.call(this, ["success", "error"]);
+  this._is_a = "bbop-manager-sparql";
+  var anchor = this;
+
+  // Endpoint.
+  anchor._endpoint = endpoint;
+
+  // New prefixes.
+  anchor._prefixes = [];
+  if (prefixes && us.isArray(prefixes)) {
+    anchor._prefixes = prefixes;
+  }
+
+  // Are we using this for the manager, or just ths template engine?
+  anchor._manager_p = true;
+  if (!engine || !response_handler) {
+    anchor._manager_p = false;
+  } else {
+    anchor._engine = engine;
+    anchor._mode = mode;
+    anchor._runner = function (resource, payload) {
+      // console.log('resource', resource);
+      // console.log('payload', payload);
+      var ret = null;
+      if (anchor._mode === "sync") {
+        ret = anchor._engine.fetch(resource, payload);
+      } else if (anchor._mode === "async") {
+        ret = anchor._engine.start(resource, payload);
+      } else {
+        throw new Error('"mode" not set in new bbop-manager-minerva');
+      }
+      return ret;
+    };
+  }
+
+  // How to deal with failure.
+  function _on_fail(resp, man) {
+    var retval = null;
+
+    // See if we got any traction.
+    if (!resp || !resp.message_type() || !resp.message()) {
+      // Something dark has happened, try to put something
+      // together.
+      // console.log('bad resp!?: ', resp);
+      var resp_seed = {
+        message_type: "error",
+        message: "deep manager error",
+      };
+      resp = new response_handler(resp_seed);
+      retval = resp;
+    }
+    anchor.apply_callbacks("error", [resp, anchor]);
+
+    return retval;
+  }
+  if (anchor._manager_p) {
+    anchor._engine.register("error", _on_fail);
+  }
+
+  // When we have nominal success, we still need to do some kind of
+  // dispatch to the proper functionality.
+  function _on_nominal_success(resp, man) {
+    var retval = resp;
+    anchor.apply_callbacks("success", [resp, anchor]);
+
+    return retval;
+  }
+  if (anchor._manager_p) {
+    anchor._engine.register("success", _on_nominal_success);
+  }
+};
+bbop.extend(manager, registry);
+
+/**
+ * Get/set the endpoint.
+ *
+ * @param {String} endpoint - a string for the endpoint URL.
+ * @returns {String} current value
+ */
+manager.prototype.endpoint = function (endpoint) {
+  var anchor = this;
+
+  if (endpoint && us.isString(endpoint)) {
+    anchor._endpoint = endpoint;
+  }
+
+  return anchor._endpoint;
+};
+
+/**
+ * Get/set the prefixes.
+ *
+ * @param {Array} prefixes - a list of array pairs (e.g. [['wdt', '<http://www.wikidata.org/prop/direct/>'], ...]) that will be added to all queries, whether a template is used or not.
+ * @returns {Array} current value(s), as array of pairs.
+ */
+manager.prototype.prefixes = function (prefixes) {
+  var anchor = this;
+
+  if (prefixes && us.isArray(prefixes)) {
+    anchor._prefixes = prefixes;
+  }
+
+  return anchor._prefixes;
+};
+
+/**
+ * Add a prefix.
+ *
+ * @param {String} prefix - prefix
+ * @param {String} expansion - expansion
+ * @returns {Array} current value(s), as array of pairs.
+ */
+manager.prototype.add_prefix = function (prefix, expansion) {
+  var anchor = this;
+
+  if (prefix && us.isString(prefix) && expansion && us.isString(expansion)) {
+    anchor._prefixes.push([prefix, expansion]);
+  }
+
+  return anchor._prefixes;
+};
+
+/**
+ * Attempt to query using the string. For shorter queries, try GET
+ * (some systems will cache these), for longer, fall back on POST.
+ *
+ * This is the core operator for this subclass. Any prefixes() will be
+ * appended.
+ *
+ * @param {String} string - the SPARQL query string
+ * @returns {Object} response
+ */
+manager.prototype.query = function (string) {
+  var anchor = this;
+
+  // Choke if somebody trie to
+  if (!anchor._manager_p) {
+    throw new Error("This manager was not provided any engine or response," + " so cannot query.");
+  }
+
+  // Add any prefixes in the object.
+  var prefixes = "";
+  //console.log('prefixes', anchor.prefixes());
+  us.each(anchor.prefixes(), function (prefix) {
+    prefixes += "PREFIX " + prefix[0] + ":" + prefix[1] + "\n";
+  });
+
+  var qstr = prefixes + string;
+  //console.log('query: qstr', qstr);
+  if (qstr.length > 1024) {
+    anchor._engine.method("GET");
+  } else {
+    anchor._engine.method("POST");
+  }
+
+  var pay = { query: qstr };
+  return anchor._runner(anchor.endpoint(), pay);
+};
+
+/**
+ * Attempt to query using a ??? template.
+ * Binding variables into a YAML mustache template.
+ *
+ * Any prefixes() will be appended.
+ *
+ * @param {String} template_as_string - the SPARQL query string
+ * @param {Object} bindings - variables to fill out the YAML template (mustache)
+ * @returns {Object} response
+ */
+manager.prototype.template = function (template_as_something, bindings) {
+  var anchor = this;
+
+  var obj = null;
+  if (us.isString(template_as_something)) {
+    // See if string string or YAML string.
+    try {
+      var pre_obj = yaml.parse(template_as_something);
+      if (pre_obj["query"]) {
+        obj = pre_obj;
+      } else {
+        // Not proper--maybe string string.
+        obj = { query: template_as_something };
+      }
+    } catch (e) {
+      // Not proper--maybe string string.
+      obj = { query: template_as_something };
+    }
+  } else if (
+    us.isObject(template_as_something) &&
+    !us.isFunction(template_as_something) &&
+    template_as_something["query"]
+  ) {
+    obj = template_as_something;
+  } else if (us.isObject(template_as_something) && !us.isFunction(template_as_something)) {
+    throw new Error("does not support this object format");
+  } else {
+    throw new Error("does not support this template format");
+  }
+
+  //
+  //console.log('template: obj', obj);
+  var filled_query_template = mustache.render(obj["query"], bindings);
+  //console.log('template: filled_query_template', filled_query_template);
+
+  // Add any prefixes in the "yaml".
+  var prefixes = "";
+  //console.log('prefixes', anchor.prefixes());
+  us.each(obj["prefixes"], function (prefix) {
+    prefixes += "PREFIX " + prefix["prefix"] + ":" + prefix["expansion"] + " ";
+  });
+
+  var qstr = prefixes + filled_query_template;
+  //console.log('template: qstr', qstr);
+  var ret = null;
+  if (anchor._manager_p) {
+    ret = anchor.query(qstr);
+  } else {
+    ret = qstr;
+  }
+
+  return ret;
+};
+
+///
+/// Exportable body.
+///
+
+export default manager;
---
+++ packages/bbop-manager-sparql/src/manager.test.js
@@ -0,0 +1,232 @@
+import { readFileSync } from "node:fs";
+import { describe, it } from "node:test";
+import { assert } from "chai";
+
+import manager from "./manager.js";
+
+function loadFixture(name) {
+  return readFileSync(new URL(`./fixtures/${name}`, import.meta.url), "utf8");
+}
+
+function makeResponse(seed) {
+  return {
+    message_type: function () {
+      return seed.message_type;
+    },
+    message: function () {
+      return seed.message;
+    },
+    raw: function () {
+      return seed.raw || null;
+    },
+  };
+}
+
+function makeResponseHandler() {
+  return function ResponseHandler(seed) {
+    this._seed = seed;
+    this.message_type = function () {
+      return (seed && (seed.message_type || seed["message_type"])) || null;
+    };
+    this.message = function () {
+      return (seed && seed.message) || null;
+    };
+  };
+}
+
+function makeEngine() {
+  var callbacks = {};
+  var calls = [];
+  var currentMethod = null;
+
+  return {
+    calls: calls,
+    callbacks: callbacks,
+    register: function (name, fn) {
+      callbacks[name] = fn;
+    },
+    method: function (nextMethod) {
+      if (typeof nextMethod !== "undefined") {
+        currentMethod = nextMethod;
+      }
+      return currentMethod;
+    },
+    fetch: function (resource, payload) {
+      calls.push({ kind: "fetch", resource: resource, payload: payload, method: currentMethod });
+      return payload;
+    },
+    start: function (resource, payload) {
+      calls.push({ kind: "start", resource: resource, payload: payload, method: currentMethod });
+      return Promise.resolve(payload);
+    },
+  };
+}
+
+describe("bbop-manager-sparql state", function () {
+  it("gets and sets endpoint and prefixes", function () {
+    var m = new manager("https://example.org/sparql", [["wd", "<http://x/>"]]);
+
+    assert.equal(m.endpoint(), "https://example.org/sparql");
+    m.endpoint("https://example.net/query");
+    assert.equal(m.endpoint(), "https://example.net/query");
+
+    assert.deepEqual(m.prefixes(), [["wd", "<http://x/>"]]);
+    m.add_prefix("wdt", "<http://y/>");
+    assert.deepEqual(m.prefixes(), [
+      ["wd", "<http://x/>"],
+      ["wdt", "<http://y/>"],
+    ]);
+  });
+});
+
+describe("bbop-manager-sparql query dispatch", function () {
+  it("uses POST for shorter queries and prepends configured prefixes", function () {
+    var engine = makeEngine();
+    var ResponseHandler = makeResponseHandler();
+    var m = new manager(
+      "https://example.org/sparql",
+      [["wd", "<http://www.wikidata.org/entity/>"]],
+      ResponseHandler,
+      engine,
+      "sync",
+    );
+
+    var result = m.query("SELECT * WHERE { ?s ?p ?o . }");
+
+    assert.equal(engine.calls.length, 1);
+    assert.equal(engine.calls[0].kind, "fetch");
+    assert.equal(engine.calls[0].method, "POST");
+    assert.equal(engine.calls[0].resource, "https://example.org/sparql");
+    assert.equal(
+      engine.calls[0].payload.query,
+      "PREFIX wd:<http://www.wikidata.org/entity/>\nSELECT * WHERE { ?s ?p ?o . }",
+    );
+    assert.equal(result, engine.calls[0].payload);
+  });
+
+  it("uses GET for longer queries", function () {
+    var engine = makeEngine();
+    var ResponseHandler = makeResponseHandler();
+    var m = new manager("https://example.org/sparql", [], ResponseHandler, engine, "sync");
+    var longQuery = "SELECT * WHERE { " + "?s ?p ?o . ".repeat(120) + "}";
+
+    m.query(longQuery);
+
+    assert.equal(engine.calls.length, 1);
+    assert.equal(engine.calls[0].method, "GET");
+  });
+
+  it("throws if query is attempted without a transport engine", function () {
+    var m = new manager("https://example.org/sparql", []);
+
+    assert.throws(function () {
+      m.query("SELECT * WHERE { ?s ?p ?o . }");
+    }, /cannot query/);
+  });
+});
+
+describe("bbop-manager-sparql templates", function () {
+  it("renders a plain string template with inline prefixes", function () {
+    var m = new manager();
+    var output = m.template(loadFixture("template-01.yaml"), { pmid: "999" });
+
+    assert.equal(
+      output,
+      'PREFIX wd: <http://www.wikidata.org/entity/> PREFIX wdt: <http://www.wikidata.org/prop/direct/>\nSELECT ?rtcl ?title ?author ?journal ?date WHERE {  ?rtcl wdt:P698 "999".\n  OPTIONAL { ?rtcl wdt:P1476 ?title. }\n  OPTIONAL { ?rtcl wdt:P2093 ?author. }\n  OPTIONAL { ?rtcl wdt:P1433 ?journal. }\n  OPTIONAL { ?rtcl wdt:P577 ?date. }\n}\n',
+    );
+  });
+
+  it("renders a template with manager-provided prefixes when yaml has none", function () {
+    var m = new manager(undefined, [
+      ["wd", "<http://www.wikidata.org/entity/>"],
+      ["wdt", "<http://www.wikidata.org/prop/direct/>"],
+    ]);
+    var output = m.template(loadFixture("template-02.yaml"), { pmid: "999" });
+
+    assert.equal(
+      output,
+      'SELECT ?rtcl ?title ?author ?journal ?date WHERE {  ?rtcl wdt:P698 "999".\n  OPTIONAL { ?rtcl wdt:P1476 ?title. }\n  OPTIONAL { ?rtcl wdt:P2093 ?author. }\n  OPTIONAL { ?rtcl wdt:P1433 ?journal. }\n  OPTIONAL { ?rtcl wdt:P577 ?date. }\n}\n',
+    );
+  });
+
+  it("renders object and yaml prefix templates exactly as the legacy code produces", function () {
+    var m = new manager();
+
+    assert.equal(
+      m.template(loadFixture("template-03.yaml"), { pmid: "999" }),
+      'PREFIX wd:<http://www.wikidata.org/entity/> PREFIX wdt:<http://www.wikidata.org/prop/direct/> SELECT ?rtcl ?title ?author ?journal ?date WHERE {  ?rtcl wdt:P698 "999".\n  OPTIONAL { ?rtcl wdt:P1476 ?title. }\n  OPTIONAL { ?rtcl wdt:P2093 ?author. }\n  OPTIONAL { ?rtcl wdt:P1433 ?journal. }\n  OPTIONAL { ?rtcl wdt:P577 ?date. }\n}\n',
+    );
+
+    assert.equal(
+      m.template(loadFixture("template-04.yaml"), { pmid: "999" }),
+      'PREFIX wd:<http://www.wikidata.org/entity/> PREFIX wdt:<http://www.wikidata.org/prop/direct/> SELECT ?rtcl ?title ?author ?journal ?date\nWHERE\n{\n  ?rtcl wdt:P698 "999".\n  OPTIONAL { ?rtcl wdt:P1476 ?title. }\n  OPTIONAL { ?rtcl wdt:P2093 ?author. }\n  OPTIONAL { ?rtcl wdt:P1433 ?journal. }\n  OPTIONAL { ?rtcl wdt:P577 ?date. }\n}\n',
+    );
+  });
+
+  it("renders template variables inside graph patterns", function () {
+    var m = new manager();
+    var output = m.template(loadFixture("template-05.yaml"), { model_id: "gomodel:123" });
+
+    assert.equal(
+      output,
+      "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\nPREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\nPREFIX gomodel: <http://model.geneontology.org/#>\nSELECT * WHERE {\n  GRAPH { gomodel:123\n    ?sub ?pred ?obj .\n  }\n}\nLIMIT 20\n",
+    );
+  });
+
+  it("supports object templates and rejects unsupported object formats", function () {
+    var m = new manager();
+    var output = m.template(
+      {
+        prefixes: [{ prefix: "ex", expansion: "<http://example.org/>" }],
+        query: "SELECT * WHERE { ex:{{ id }} ?p ?o . }",
+      },
+      { id: "foo" },
+    );
+
+    assert.equal(output, "PREFIX ex:<http://example.org/> SELECT * WHERE { ex:foo ?p ?o . }");
+    assert.throws(function () {
+      m.template({ title: "no query field" }, {});
+    }, /does not support this object format/);
+  });
+});
+
+describe("bbop-manager-sparql callback wiring", function () {
+  it("forwards success and error callbacks from the engine", function () {
+    var engine = makeEngine();
+    var ResponseHandler = makeResponseHandler();
+    var m = new manager("https://example.org/sparql", [], ResponseHandler, engine, "sync");
+    var seen = [];
+
+    m.register("success", function (resp) {
+      seen.push(["success", resp.message()]);
+    });
+    m.register("error", function (resp) {
+      seen.push(["error", resp.message()]);
+    });
+
+    engine.callbacks.success(makeResponse({ message_type: "success", message: "ok" }), m);
+    engine.callbacks.error(makeResponse({ message_type: "error", message: "boom" }), m);
+
+    assert.deepEqual(seen, [
+      ["success", "ok"],
+      ["error", "boom"],
+    ]);
+  });
+
+  it("wraps missing error responses through the provided response handler", function () {
+    var engine = makeEngine();
+    var ResponseHandler = makeResponseHandler();
+    var m = new manager("https://example.org/sparql", [], ResponseHandler, engine, "sync");
+    var seen = null;
+
+    m.register("error", function (resp) {
+      seen = resp;
+    });
+
+    engine.callbacks.error(null, m);
+
+    assert.isNotNull(seen);
+    assert.equal(seen.message_type(), "error");
+    assert.equal(seen.message(), "deep manager error");
+  });
+});
